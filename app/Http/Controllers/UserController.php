8c9655cd94570a36e057a71b4f0dd40f26a6415c
s96Abrar <s96mini.cube@gmail.com>
2023-06-05 11:23:41 +0600

    Updated routes and user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = auth()->user();
+        $users = User::query();
+        if ($user->isChairman()) {
+            $users->where('role_id', 3); // Get only teachers
+        }
+        $users = $users->where('id', '!=', auth()->id())->where('role_id', '!=', 4)->with(['role', 'department'])->latest()->paginate(15);
+        return view('backend.users.index', compact('users'));
+    }
+
+    public function show(User $user)
+    {
+        return view('backend.users.show', compact('user'));
+    }
+
+    public function changeRole(User $user)
+    {
+        $roles = Role::pluck('title', 'id')->toArray();
+        unset($roles[1]);
+        unset($roles[4]);
+        return view('backend.users.change_role', compact('roles', 'user'));
+    }
+
+    public function updateRole(Request $request, User $user)
+    {
+        $user->update([
+            'role_id' => $request->role_id
+        ]);
+
+        return redirect()
+            ->route('users.index')
+            ->withMessage('Successfully Updated Role');
+    }
+
+    public function approve(User $user)
+    {
+        $user->update([
+            'approved_at' => now(),
+        ]);
+        return redirect()
+            ->route('users.index')
+            ->withMessage('Successfully approved ' . $user->name);
+    }
+
+    public function disapprove(User $user)
+    {
+        // dd($user);
+        $user->update([
+            'approved_at' => null,
+        ]);
+        return redirect()
+            ->route('users.index')
+            ->withMessage('Successfully disapproved ' . $user->name);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('backend.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Users
+    Route::prefix('users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
+        Route::patch('/{user}/approve', [UserController::class, 'approve'])->name('users.approve');
+        Route::patch('/{user}/disapprove', [UserController::class, 'disapprove'])->name('users.disapprove');
+        Route::get('/{user}/change-role', [UserController::class, 'changeRole'])->name('users.change_role');
+        Route::patch('/{user}/change-role', [UserController::class, 'updateRole'])->name('users.update_role');
+    });
 });
 
 require __DIR__.'/auth.php';
+
+Route::redirect('/', '/login');
