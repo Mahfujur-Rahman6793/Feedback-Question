ac1d5b889975c59557194d58dc1f134d1c2b4502
s96Abrar <s96mini.cube@gmail.com>
2023-06-08 21:47:31 +0600

    Few fixes

--- resources/views/components/pages/actionDelete.blade.php
+++ resources/views/components/pages/actionDelete.blade.php
@@ -7,7 +7,8 @@
         class="btn btn-outline-danger btn-sm"
         type="submit"
         onclick="return confirm('Are you sure to {{ strtolower($title) }}?')">
-        <i class="bi bi-trash"></i>
-        @if (!$iconOnly) {{ $title }} @endif
+        {{-- <i class="bi bi-trash"></i> --}}
+        {{-- @if (!$iconOnly) {{ $title }} @endif --}}
+        {{ $title }}
     </button>
 </form>
