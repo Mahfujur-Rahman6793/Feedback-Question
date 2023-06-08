1bd566247c2025990313d1e65cba98473fcab3ef
s96Abrar <s96mini.cube@gmail.com>
2023-06-08 21:49:22 +0600

    Added relationship in models

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    use HasFactory;
+    protected $guarded = [];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function scale()
+    {
+        return $this->belongsTo(Scale::class);
+    }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function questionType()
+    {
+        return $this->belongsTo(QuestionType::class);
+    }
 }
