cdd942ce3f3b7196e735274a40214d04eb136cb2
s96Abrar <s96mini.cube@gmail.com>
2023-06-09 17:46:51 +0600

    Update dashboard view with dynamic data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\Feedback;
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.dashboard');
+        $user = auth()->user();
+        $total_question = 0;
+        $scenario_question = 0;
+        $chapter_question = 0;
+        $teacher_rating = 'N/A';
+
+        $chairman_count = 0;
+
+        $course_count = 0;
+        $teacher_count = 0;
+
+        $requests = [];
+
+        $scaleLabels = [
+            0 => 'N/A',
+            1 => 'Very bad',
+            2 => 'Bad',
+            3 => 'Average',
+            4 => 'Good',
+            5 => 'Very good',
+        ];
+
+        if ($user->isTeacher()) {
+            $questions = Question::select('question_type_id', DB::raw('COUNT(*) as total'))->groupBy('question_type_id')->get();
+            foreach ($questions as $question) {
+                if ($question->question_type_id == 1) { // Chapter
+                    $chapter_question = $question->total;
+                } else if ($question->question_type_id == 2) { // Scenario
+                    $scenario_question = $question->total;
+                }
+            }
+            $total_question = $chapter_question + $scenario_question;
+
+            $feedback = Feedback::select('feedback.course_id', DB::raw('AVG(scales.value) as rating'))
+                            ->join('users', 'feedback.user_id', '=', 'users.id')
+                            ->join('scales', 'feedback.scale_id', '=', 'scales.id')
+                            ->join('courses', 'feedback.course_id', '=', 'courses.id')
+                            ->where('users.id', auth()->id())
+                            ->groupBy('feedback.course_id')->first()?->rating ?? 0;
+            $teacher_rating = $scaleLabels[intval($feedback)];
+        } else if ($user->isAdmin()) {
+            $chairman_count = User::where('role_id', User::TYPE_CHAIRMAN)->get()->count();
+        }
+
+        if ($user->isStudent() || $user->isChairman()) {
+            $course_count = Course::where('department_id', $user->department_id)->count();
+            $teacher_count = User::where('role_id', User::TYPE_TEACHER)->get()->count();
+        }
+
+        if ($user->isAdmin() || $user->isChairman()) {
+            $type = User::TYPE_TEACHER;
+            if ($user->isAdmin()) {
+                $type = User::TYPE_CHAIRMAN;
+            }
+
+            $requests = User::whereNull('approved_at')->where('role_id', $type)->with(['department', 'role'])->latest()->take(5)->get();
+        }
+
+        return view('backend.dashboard',
+                    compact(
+                        'total_question', 'scenario_question', 'chapter_question', 'teacher_rating',
+                        'chairman_count',
+                        'course_count', 'teacher_count',
+                        'requests',
+                    ));
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -35,7 +35,7 @@
                                         <i class="bi bi-chat-dots"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>145</h6>
+                                        <h6>{{ $total_question }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -51,7 +51,7 @@
                                         <i class="bi bi-chat-dots"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>14</h6>
+                                        <h6>{{ $chapter_question }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -67,7 +67,7 @@
                                         <i class="bi bi-chat-dots"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>131</h6>
+                                        <h6>{{ $scenario_question }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -83,7 +83,7 @@
                                         <i class="bi bi-people"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>131</h6>
+                                        <h6>{{ $teacher_rating }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -102,7 +102,7 @@
                                         <i class="bi bi-people"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>64</h6>
+                                        <h6>{{ $chairman_count }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -121,7 +121,7 @@
                                         <i class="bi bi-journal-text"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>64</h6>
+                                        <h6>{{ $course_count }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -137,7 +137,7 @@
                                         <i class="bi bi-people"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>64</h6>
+                                        <h6>{{ $teacher_count }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -157,6 +157,7 @@
                                 <table class="table table-borderless datatable">
                                     <thead>
                                         <tr>
+                                            <th scope="col">#</th>
                                             <th scope="col">Teacher</th>
                                             <th scope="col">E-mail</th>
                                             <th scope="col">Department</th>
@@ -164,10 +165,16 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @forelse ([] as $user)
-
+                                        @forelse ($requests as $user)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ $user->department->name }}</td>
+                                            <td>{{ $user->role->title }}</td>
+                                        </tr>
                                         @empty
-                                        <tr><td colspan="4">No latest requests</td></tr>
+                                        <tr><td colspan="5">No latest requests</td></tr>
                                         @endforelse
                                     </tbody>
                                 </table>
