e186227979a1c4770b29b73439ef3e141be65f52
s96Abrar <s96mini.cube@gmail.com>
2023-06-10 18:06:39 +0600

    Added tinymce

---
+++ resources/views/components/head/tinymce-config.blade.php
@@ -0,0 +1,11 @@
+<script src="https://cdn.tiny.cloud/1/zh5em4s6ud80izf1tdo31rw7krelmnwb4r66cs90j3tgwm38/tinymce/6/tinymce.min.js"
+    referrerpolicy="origin"></script>
+
+<script>
+    tinymce.init({
+        selector: 'textarea.tinymce',
+        plugins: 'code table lists',
+        browser_spellcheck: true,
+        toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+    });
+</script>
