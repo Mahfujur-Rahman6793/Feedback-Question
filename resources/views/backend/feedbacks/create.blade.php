a4cf4593e7f5f88fc8e8575a55828875b1a35ec0
s96Abrar <s96mini.cube@gmail.com>
2023-06-17 17:27:28 +0600

    Fixed provide feedbacks for own department only

--- resources/views/backend/feedbacks/create.blade.php
+++ resources/views/backend/feedbacks/create.blade.php
@@ -12,7 +12,7 @@
             <h5 class="card-title"></h5>
             <x-forms.form :action="route('feedbacks.store')" method="POST" class="row px-3 py-3">
                 <x-forms.select name="course" label="Select course" :options="$courses" :selected="old('course')" required />
-                <x-forms.select name="teacher" label="Select teacher" :options="$teachers" :selected="old('teacher')" required />
+                <x-forms.select name="teacher" label="Select teacher" :options="[]" :selected="old('teacher')" required />
                 <x-forms.select name="scale" label="Select scale" :options="$scales" :selected="old('scale')" required />
 
                 <x-forms.textarea name="comment" :label="__('Enter comment')" :value="old('comment')" rows="4" required/>
@@ -23,5 +23,29 @@
             </x-forms.form>
         </div>
     </div>
+
+    @push('customjs')
+        <script>
+            function fetchTeacherList(courseVal) {
+                $('#teacherInput').empty()
+                $('#teacherInput').append("<option value='' disabled selected>Select value</option>")
+                $.each(dataSource[courseVal], function(index, item) {
+                    $('#teacherInput').append(`<option value='${index}'>${item}</option>`)
+                })
+            }
+
+            const dataSource = {!! json_encode($data) !!};
+
+            $(document).ready(function() {
+                var courseVal = $('#courseInput').val();
+
+                $('#courseInput').on('change', (e) => fetchTeacherList(e.target.value))
+
+                if (courseVal != null && courseVal.length > 0) {
+                    fetchTeacherList(courseVal)
+                }
+            })
+        </script>
+    @endpush
 
 </x-backend.master>
